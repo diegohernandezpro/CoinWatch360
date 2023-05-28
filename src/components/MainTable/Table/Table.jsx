dd758038bf569a01230bc563b4fbc3fa44e2c08f
Diego Hernandez <diegohc2003@gmail.com>
2023-05-28 17:21:18 -0600

    Added json data in case I exceed limit of max 10 api calls to coingecko pero minute

--- src/components/MainTable/Table/Table.jsx
+++ src/components/MainTable/Table/Table.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+// import jsonData from "../testData.json";
 
 import { TableContainer, StyledP, TableWrapper } from "./Table.styles";
 
@@ -21,6 +22,8 @@ export default class Table extends React.Component {
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
       );
       this.setState({ coinList: data, isLoading: false });
+
+      // this.setState({ isLoading: false, coinList: jsonData });
     } catch (error) {
       this.setState({ isLoading: false, hasError: true });
       console.log("🚀 Table ~ getCoinData= ~ error:", error);
