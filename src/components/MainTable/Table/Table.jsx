e59596057a4ca2fd51e2bf32c3a406971cc86aac
Diego Hernandez <diegohc2003@gmail.com>
2023-08-10 07:31:40 -0600

    solved conflicts with merge betweeen master and Portfolio-Page

--- src/components/MainTable/Table/Table.jsx
+++ src/components/MainTable/Table/Table.jsx
@@ -23,7 +23,7 @@ export const Table = () => {
       const { data } = await axios.get(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
       );
-      // const data = jsonData;
+      // const data = jsonData; //change
       setLoading(false);
       setCoinList(data);
     } catch (error) {
