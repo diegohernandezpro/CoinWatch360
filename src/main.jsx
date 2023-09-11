582a1ded573b085fd8cb671b95f042d969cc4e1a
Diego Hernandez <diegohc2003@gmail.com>
2023-09-11 16:36:14 -0400

    changed package.json, took away mode production, dev: vite

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store";
-import App from "./App.jsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
