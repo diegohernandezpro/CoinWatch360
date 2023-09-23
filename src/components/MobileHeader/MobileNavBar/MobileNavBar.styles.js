5b2f7d67e33674b92c2a83292a6b31e22b58c3e9
Diego Hernandez <diegohc2003@gmail.com>
2023-09-23 13:54:55 -0400

    added icons to mobile nav bar on bottom of screen

--- src/components/MobileHeader/MobileNavBar/MobileNavBar.styles.js
+++ src/components/MobileHeader/MobileNavBar/MobileNavBar.styles.js
@@ -5,5 +5,32 @@ export const Container = styled.header`
   width: Calc(100%);
   border-radius:  0  0 1rem 1rem;
   background: ${({ theme }) => theme.nested.active};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   };
+`;
+
+export const Child = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  width: Calc(100% / 4);
+  height: Calc(100% - 2rem);
+  overflow: hidden;
+`;
+
+export const IconWrapper = styled.div`
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.chart.color};
+
+  &:hover {
+    color: ${({ theme }) => theme.chart.lineColor};
+    cursor: pointer;
+  }
 `;
