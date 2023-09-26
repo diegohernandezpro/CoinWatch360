b3e1b446c1752eccaebd70aae682b6877bed8bdd
Diego Hernandez <diegohc2003@gmail.com>
2023-09-25 21:17:48 -0400

    mobile version with redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { store, persistor } from "@/modernStore";
-import { store } from "@/modernStore";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/modernStore";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
-        <App />
-        {/* </PersistGate> */}
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
